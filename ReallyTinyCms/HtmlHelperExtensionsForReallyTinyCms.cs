d52d2866f9f21c2f408e04d989d173b6eb037944
David Whitney <david@davidwhitney.co.uk>
2011-08-04 20:59:35 +0100

    init.

---
+++ ReallyTinyCms/HtmlHelperExtensionsForReallyTinyCms.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace ReallyTinyCms
+{
+    public static class HtmlHelperExtensionsForReallyTinyCms
+    {
+        public static CmsContentRepository Cms(this HtmlHelper helper)
+        {
+            return new CmsContentRepository();
+        } 
+    }
+}
