21ea348c0533027d4172b865ce074a3f15792840
David Whitney <david@davidwhitney.co.uk>
2011-08-18 15:38:12 +0100

    tidyup.

--- ReallyTinyCms.ExampleWebsite/Global.asax.cs
+++ ReallyTinyCms.ExampleWebsite/Global.asax.cs
@@ -7,21 +7,12 @@ using ReallyTinyCms.Core.Storage;
 
 namespace ReallyTinyCms.ExampleWebsite
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
     public class MvcApplication : System.Web.HttpApplication
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-
+			routes.MapRoute("Default", "{controller}/{action}/{id}",  new { controller = "Home", action = "Index", id = UrlParameter.Optional } );
         }
 
         protected void Application_Start()
