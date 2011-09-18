9b90d870583d12c3274a467a2af6005dd20785d5
David Whitney <david@davidwhitney.co.uk>
2011-09-18 23:03:05 +0100

    Better error if route isn't available to generate edit link.

--- ReallyTinyCms/Core/ReallyTinyCmsUiSugar.cs
+++ ReallyTinyCms/Core/ReallyTinyCmsUiSugar.cs
@@ -48,29 +48,39 @@ namespace ReallyTinyCms.Core
             {
                 if (showEditHint)
                 {
-                    var requestContext = HttpContext.Current.Request.RequestContext;
-
-                    var routeValues = new RouteValueDictionary
-                                          {
-                                              {"action", "edit"},
-                                              {"controller", "ReallyTinyCms"},
-                                              {"name", contentItemName}
-                                          };
-                    foreach(var item in HttpContext.Current.Request.QueryString.AllKeys)
+                    if (RouteTable.Routes["ReallyTinyCmsAdmin"] == null)
                     {
-                        routeValues.Add(item, HttpContext.Current.Request.QueryString[item]);
+                        throw new InvalidOperationException("Cannot auto-generate edit link unless you use ConfigureEditRoute during registration.");
                     }
 
-                    var link = HtmlHelper.GenerateRouteLink(requestContext, RouteTable.Routes, "Edit", "ReallyTinyCmsAdmin",
-                                                 routeValues, new Dictionary<string, object>{{"target", "_blank"}});
-
                     buffer.Append("<div id=\"editContentItem_" + contentItemName + "\" class=\"contentItemEditLink\">");
-                    buffer.Append(link);
+                    buffer.Append(GenerateEditLink(contentItemName));
                     buffer.Append("</div>");
                 }
             }
 
             return buffer.ToString();
+        }
+
+        private static string GenerateEditLink(string contentItemName)
+        {
+            var requestContext = HttpContext.Current.Request.RequestContext;
+
+            var routeValues = new RouteValueDictionary
+                                  {
+                                      {"action", "edit"},
+                                      {"controller", "ReallyTinyCms"},
+                                      {"name", contentItemName}
+                                  };
+
+            foreach (var item in HttpContext.Current.Request.QueryString.AllKeys)
+            {
+                routeValues.Add(item, HttpContext.Current.Request.QueryString[item]);
+            }
+
+            var link = HtmlHelper.GenerateRouteLink(requestContext, RouteTable.Routes, "Edit", "ReallyTinyCmsAdmin",
+                                                    routeValues, new Dictionary<string, object> {{"target", "_blank"}});
+            return link;
         }
 
         public bool EditEnabledForCurrentRequest()
